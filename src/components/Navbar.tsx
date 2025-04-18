8fcb3e26471c86bca2199baa0b6daa6992d74593
ItsAbhishekkashyap <abhi47025@gmail.com>
2025-04-18 08:03:20 +0530

    removing callback from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -69,7 +69,7 @@ const Navbar = () => {
                   </Button>
                 </Link>
                 <Link href="/sign-up">
-                  <Button  className="bg-purple-600 hover:bg-purple-700">
+                  <Button className="bg-purple-600 hover:bg-purple-700">
                     Sign Up
                   </Button>
                 </Link>
@@ -141,9 +141,7 @@ const Navbar = () => {
                   </span>
                 </div>
                 <Button
-                  onClick={async () => {
-                    await signOut({ redirect: true, callbackUrl: '/sign-in' });
-                  }}
+                  onClick={() => signOut()}
                   className="w-full justify-start"
                   variant="ghost"
                 >
