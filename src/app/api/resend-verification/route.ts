73c987e1bfddd33cb033620758313106f8543a5c
ItsAbhishekkashyap <abhi47025@gmail.com>
2025-04-21 17:20:14 +0530

    fixes resend verification

--- src/app/api/resend-verification/route.ts
+++ src/app/api/resend-verification/route.ts
@@ -1,44 +1,45 @@
-// /pages/api/resend-verification.ts
-import { NextRequest, NextResponse } from "next/server";
-import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
-import { generateVerificationCode } from "@/lib/generateVerificationCode";
-import { db } from "@/lib/db"; // adjust according to your db setup
-import { ApiResponse } from "@/types/ApiResponse";
+// // /pages/api/resend-verification.ts
+// import { NextRequest, NextResponse } from "next/server";
+// import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+// import { generateVerificationCode } from "@/lib/generateVerificationCode";
+// // adjust according to your db setup
+// import { ApiResponse } from "@/types/ApiResponse";
+// import dbConnect from "@/lib/dbconnect";
 
-export async function POST(req: Request): Promise<NextResponse<ApiResponse>> {
-  const { username } = await req.json();
+// export async function POST(req: Request): Promise<NextResponse<ApiResponse>> {
+//   const { username } = await req.json();
 
-  if (!username) {
-    return NextResponse.json(
-      { success: false, message: "Username is required" },
-      { status: 400 }
-    );
-  }
+//   if (!username) {
+//     return NextResponse.json(
+//       { success: false, message: "Username is required" },
+//       { status: 400 }
+//     );
+//   }
 
-  const user = await db.user.findUnique({
-    where: { username },
-  });
+//   const user = await dbConnect.user.findUnique({
+//     where: { username },
+//   });
 
-  if (!user || !user.email) {
-    return NextResponse.json(
-      { success: false, message: "User not found" },
-      { status: 404 }
-    );
-  }
+//   if (!user || !user.email) {
+//     return NextResponse.json(
+//       { success: false, message: "User not found" },
+//       { status: 404 }
+//     );
+//   }
 
-  const newCode = generateVerificationCode();
+//   const newCode = generateVerificationCode();
 
-  await db.verificationCode.upsert({
-    where: { username },
-    update: { code: newCode },
-    create: { username, code: newCode },
-  });
+//   await dbConnect.verificationCode.upsert({
+//     where: { username },
+//     update: { code: newCode },
+//     create: { username, code: newCode },
+//   });
 
-  const emailResponse = await sendVerificationEmail(
-    user.email,
-    username,
-    newCode
-  );
+//   const emailResponse = await sendVerificationEmail(
+//     user.email,
+//     username,
+//     newCode
+//   );
 
-  return NextResponse.json(emailResponse);
-}
+//   return NextResponse.json(emailResponse);
+// }
